f8e98bc6af2a8a7ccd91461a3be40838b8b47904
JD <janet_dornan@hotmail.com>
2023-01-07 12:24:28 +0000

    Add event listener for DOMContentLoaded to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,112 +2,119 @@
 // Inspired by the 'Whac-A-Mole' game by Ania Kubow
 // https://www.freecodecamp.org/news/learn-javascript-by-building-7-games-video-course/
 
-// game object
-let game = {
-    score: 0,
-    numberOfTiles: 9,
-    timePeriod: 60,
-};
-/* istanbul ignore next */
+// DOM event listener - all HTML elements to be read before loading JS file
+// makes sure events happen in order
+// Had issues with Jest testing as DOM hadnt loaded
+document.addEventListener('DOMContentLoaded', () => {
 
-// get the game elements with their classes and ids
-const grid = document.querySelector('.grid');
-const target = document.querySelector('.target');
-const timeLeft = document.querySelector('#time-left');
-const scoreDisplayed = document.querySelector('#score');
+    // game object
+    let game = {
+        score: 0,
+        numberOfTiles: 9,
+        timePeriod: 60,
+    };
 
-const start = document.querySelector('#start-game');
-const reset = document.querySelector('#reset-game');
+    // get the game elements with their classes and ids
+    const grid = document.querySelector('.grid');
+    // const target = document.querySelector('.target');
+    const timeLeft = document.querySelector('#time-left');
+    const scoreDisplayed = document.querySelector('#score');
 
-// game variables
-let hitPosition;
-let randomTargetTimer;
-let countDownTimer;
+    const start = document.querySelector('#start-game');
+    const reset = document.querySelector('#reset-game');
 
-// create the game grid
-function createGrid() {
-    for (let n = 0; n < game.numberOfTiles; n++) {
-        // create the <div> tag for each tile
-        const tile = document.createElement('div');
-        tile.classList.add('tile');
-        tile.setAttribute('id', n); // each tile gets it's 'id'. Also use 'n' for looping
-        grid.appendChild(tile);
+    // game variables
+    let hitPosition;
+    let randomTargetTimer;
+    let countDownTimer;
+
+    function createGrid() {
+        // create the game grid
+        for (let n = 0; n < game.numberOfTiles; n++) {
+            // create the <div> tag for each tile
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
+            tile.setAttribute('id', n); // each tile gets it's 'id'. Also use 'n' for looping
+            grid.appendChild(tile);
+        }
     }
-}
-createGrid();
-const tiles = document.querySelectorAll('.tile'); // select all elements with class .tile
 
-// generate random target tile
-function randomTarget() {
-    // get each tile (0-8) and remove the class .target
-    tiles.forEach(tile => {
-        tile.classList.remove('target');
-    });
-    let randomNumber = Math.floor(Math.random() * 9) //0-8
-    let randomTile = tiles[randomNumber];
-    randomTile.classList.add('target');
-    hitPosition = randomTile.id;
-}
+    createGrid();
+    const tiles = document.querySelectorAll('.tile'); // select all elements with class .tile
 
-// countdown timer and check if game is over
-function countDown() {
-    game.timePeriod--  //decrement
-    timeLeft.textContent = game.timePeriod;
-
-    if (game.timePeriod == 0) {
-        clearInterval(countDownTimer);
-        clearInterval(randomTargetTimer);
-        //end game
-        gameOver();
-    }
-}
-
-// check if random target has been clicked or touched
-function isTargetHit() {
-    tiles.forEach(tile => {
-        tile.addEventListener('click', () => {
-            // 'click' for mouse click or touch on touchscreen
-            if (tile.id == hitPosition) {
-                game.score++;
-                scoreDisplayed.textContent = game.score;
-                hitPosition = null;
-            } // if you hit the pink box
+    // generate random target tile
+    function randomTarget() {
+        // get each tile (0-8) and remove the class .target
+        tiles.forEach(tile => {
+            tile.classList.remove('target');
         });
-    });
-}
-
-// reset game when Reset button is clicked/touched
-function resetGame() {
-    reset.onclick = () => {
-        clearInterval(countDownTimer);
-        clearInterval(randomTargetTimer);
-        scoreDisplayed.textContent = 0;
-        timeLeft.textContent = 60;
-    }     
-}
-resetGame();
-
-function startGame() {
-    // start game when Start button is clicked/touched
-    start.onclick = () => {
-        game.score = 0;
-        game.timePeriod = 60;
-        isTargetHit();
-        randomTargetTimer = setInterval(randomTarget, 1000); //every 1.0 sec for Level-1
-        countDownTimer = setInterval(countDown, 1000); // every 1 sec
+        let randomNumber = Math.floor(Math.random() * 9) //0-8
+        let randomTile = tiles[randomNumber];
+        randomTile.classList.add('target');
+        hitPosition = randomTile.id;
     }
-}
-startGame();
 
-function gameOver() {    
-    // create the text 'Game Over'
-    const gameOverText = document.querySelector('#game-over');
-    const gameOverDisplay = document.createElement('span');
-    gameOverDisplay.textContent = "Game Over!";
-    gameOverDisplay.style.color = 'red';
-    gameOverText.appendChild(gameOverDisplay);
-}
+    // countdown timer and check if game is over
+    function countDown() {
+        game.timePeriod--  //decrement
+        timeLeft.textContent = game.timePeriod;
+
+        if (game.timePeriod == 0) {
+            clearInterval(countDownTimer);
+            clearInterval(randomTargetTimer);
+            //end game
+            gameOver();
+        }
+    }
+
+    // check if random target has been clicked or touched
+    function isTargetHit() {
+        tiles.forEach(tile => {
+            tile.addEventListener('click', () => {
+                // 'click' for mouse click or touch on touchscreen
+                if (tile.id == hitPosition) {
+                    game.score++;
+                    scoreDisplayed.textContent = game.score;
+                    hitPosition = null;
+                } // if you hit the pink box
+            });
+        });
+    }
+
+    // reset game when Reset button is clicked/touched
+    function resetGame() {
+        reset.onclick = () => {
+            clearInterval(countDownTimer);
+            clearInterval(randomTargetTimer);
+            scoreDisplayed.textContent = 0;
+            timeLeft.textContent = 60;
+        }     
+    }
+    resetGame();
+
+    function startGame() {
+        // start game when Start button is clicked/touched
+        start.onclick = () => {
+            game.score = 0;
+            game.timePeriod = 60;
+            isTargetHit();
+            randomTargetTimer = setInterval(randomTarget, 1000); //every 1.0 sec for Level-1
+            countDownTimer = setInterval(countDown, 1000); // every 1 sec
+        }
+    }
+    startGame();
+
+    function gameOver() {    
+        // create the text 'Game Over'
+        const gameOverText = document.querySelector('#game-over');
+        const gameOverDisplay = document.createElement('span');
+        gameOverDisplay.textContent = "Game Over!";
+        gameOverDisplay.style.color = 'red';
+        gameOverText.appendChild(gameOverDisplay);
+    }
+
+}) 
 
 // END of file - place module exports here
 // remember to import these into app.test.js
-module.exports = { game };
+module.exports = { game, createGrid };
--- assets/scripts/tests/app.test.js
+++ assets/scripts/tests/app.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-
+let createGrid;
 const { game } = require("../app"); // app.js file
 
 /*
@@ -9,7 +9,10 @@ Jest Spy to check if alert has been called.
 First argument to spyOn is the window and  the second is the name of the method, in this case "alert".
 So we're going to catch it when an alert happens  and divert it harmlessly into an empty function. 
 */
-jest.spyOn(window, "alert").mockImplementation(() => { });
+//jest.spyOn(window, "alert").mockImplementation(() => { });
+
+// ARE YOU STRUGGLING WITH NULL VALUES IN JEST?
+// https://code-institute-room.slack.com/archives/C7HD37Q1F/p1650618994260909
 
 // Retro Game
 beforeAll(() => {
@@ -18,9 +21,12 @@ beforeAll(() => {
     document.open();
     document.write(fileContents);
     document.close();
+    createGrid = require("../app"); // app.js file
 });
 
-//check for game score and level
+
+
+//check game object
 describe("game object contains correct keys and default values", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
@@ -40,4 +46,13 @@ describe("game object contains correct keys and default values", () => {
     test("time period key exists", () => {
         expect(game.timePeriod).toEqual(60);
     });
+});
+
+
+// check grid is created correctly
+describe("createGrid works correctly", () => {
+    beforeAll(() => {
+        createGrid();
+    });
+    
 });
