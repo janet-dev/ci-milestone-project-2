b5d3e36fef7d18259af6de5d2f0b210a38d89e4a
JD <janet_dornan@hotmail.com>
2023-01-14 16:40:35 +0000

    Clear form after submission

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -2,6 +2,8 @@
 
 function sendMail(contactForm) {
     const formSubmitted = document.querySelector('#form-submit');
+    const sendIdea = document.querySelector('#send-idea');
+
     emailjs.send("service_lo01bwk", "game-suggestion-template", {
         from_name: contactForm.name.value,
         message: contactForm.message.value,
@@ -12,10 +14,12 @@ function sendMail(contactForm) {
                 console.log("SUCCESS", response.status, response.text);
                 // create the text 'Idea submitted - thank you!'
                 formSubmitted.textContent = "Idea submitted!";
+                sendIdea.reset();
             },
             function(error) {
                 console.log("FAILED", error);
                 formSubmitted.textContent = "Submission failed!";
+                sendIdea.reset();
             }
         );
         return false; // To block from loading a new page
