721b575203151161d327618aa79689b5c77e5600
JD <janet_dornan@hotmail.com>
2023-01-14 17:13:12 +0000

    Add disable property to email send button

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -1,9 +1,12 @@
 // EmailJS
 
+const submitButton = document.querySelector('#submit-button');
+submitButton.disabled = false;
+
 function sendMail(contactForm) {
     const formSubmitted = document.querySelector('#form-submit');
     const sendIdea = document.querySelector('#send-idea');
-
+    
     emailjs.send("service_lo01bwk", "game-suggestion-template", {
         from_name: contactForm.name.value,
         message: contactForm.message.value,
@@ -15,11 +18,13 @@ function sendMail(contactForm) {
                 // create the text 'Idea submitted - thank you!'
                 formSubmitted.textContent = "Idea submitted!";
                 sendIdea.reset();
+                submitButton.disabled = true;
             },
             function(error) {
                 console.log("FAILED", error);
                 formSubmitted.textContent = "Submission failed!";
                 sendIdea.reset();
+                submitButton.disabled = true;
             }
         );
         return false; // To block from loading a new page
