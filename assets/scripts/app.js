a1416615a62503755558dcbb7dac68b6a80c8a74
JD <janet_dornan@hotmail.com>
2023-02-04 14:19:10 +0000

    Add extra comments to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,6 @@
 /**
  * 2D Retro Game - Box Hunt
  * Some code sourced from games by Ania Kubow
- * 
- * DOM event listener - all HTML elements to be read before loading JS file
- * makes sure events happen in order.
  */
 
 // game object
@@ -15,16 +12,16 @@ const GAME_CONFIG = {
 };
 
 let score = 0;
-let period = GAME_CONFIG.timePeriod;
+let period = GAME_CONFIG.timePeriod; // length of the game
 
 let grid = document.querySelector('.grid'); // get the game elements with their classes and ids
 let timeLeft = document.querySelector('#time-left');
 let scoreDisplayed = document.querySelector('#score');
 let tiles = null;
 
-let start = document.querySelector('#start-game');
-let reset = document.querySelector('#reset-game');
-// ensure when 'start' is available, 'reset' is not and vice versa
+let start = document.querySelector('#start-game'); // Play button
+let reset = document.querySelector('#reset-game'); // Replay button
+// ensure when 'Start' is available, 'Replay' is not and vice versa
 
 // game variables
 let hitPosition = null;
@@ -33,8 +30,9 @@ let randomTargetTimer = GAME_CONFIG.startPeriod;
 let countDownTimer = GAME_CONFIG.countDownPeriod;
 
 function initialiseGame() {
-    start.disabled = false;
-    reset.disabled = true;
+    // 'Start'/'Restart' buttons
+    start.disabled = false; // 'Start' is enabled
+    reset.disabled = true; // 'Restart' is disabled
 
     createGrid();
     tiles = document.querySelectorAll('.tile'); // select all elements with class .tile
@@ -43,17 +41,18 @@ function initialiseGame() {
     start.addEventListener('click', startGame);
 }
 
+// DOM event listener - all HTML elements to be read before loading JS file, as it makes sure events happen in order.
 document.addEventListener('DOMContentLoaded', initialiseGame);
 
 function createGrid() {
-    //Code from 'Build your own CANDY CRUSH using JavaScript, HTML and CSS' by Ania Kubow
-    // create the game grid
+    // code from 'Build your own CANDY CRUSH using JavaScript, HTML and CSS' by Ania Kubow
+    // create the game grid of 'n' number of tiles
     for (let n = 0; n < GAME_CONFIG.numberOfTiles; n++) {
         // create the <div> tag for each tile
         const tile = document.createElement('div');
         tile.classList.add('tile');
         tile.setAttribute('id', n); // each tile gets it's 'id'. Also use 'n' for looping
-        grid.appendChild(tile);
+        grid.appendChild(tile); // append the tile to the grid <div>
     }
 }
 
@@ -63,37 +62,39 @@ function randomTarget() {
     // code from 'Learn JavaScript by building 7 games: Whac-A-Mole' by Ania Kubow
     
     tiles.forEach(tile => {
-        tile.classList.remove('target'); // remove target before producing new one
+        // removes target before producing new one, otherwise will have several targets at once
+        tile.classList.remove('target');
     });
-    let randomNumber = Math.floor(Math.random() * 9); //generate random target tile number 0-8
-    let randomTile = tiles[randomNumber]; //generate random target tile
-    randomTile.classList.add('target'); // assign target class
+    let randomNumber = Math.floor(Math.random() * 9); //generate random number 0-8
+    let randomTile = tiles[randomNumber]; //generate random target tile position
+    randomTile.classList.add('target'); // assign target class to position
     hitPosition = randomTile.id; // assign potential hit position with tile and it's id
 }
 
 function clearTheIntervals() {
+    // clear when game has timed out for game over or when 'Replay' button is hit
     clearInterval(countDownTimer);
     clearInterval(randomTargetTimer);
 }
 
 function countDown() {
     // countdown timer and check if game is over
-    //some code from 'Learn JavaScript by building 7 games: Whac-A-Mole' by Ania Kubow
+    // some code from 'Learn JavaScript by building 7 games: Whac-A-Mole' by Ania Kubow
     period--;  //decrement
     timeLeft.textContent = period;
     if (period == 0) {
-        clearTheIntervals();
-        
+        clearTheIntervals();   
         gameOver(); //end the game
     }
 }
 
 function onTileClicked() {
-    clearInterval(randomTargetTimer); // clear repeat period
+    // if target is hit, increase the score and speed of the game
+    clearInterval(randomTargetTimer); // clear current repeat period
 
     score++;
     scoreDisplayed.textContent = score;
-    hitPosition = null;
+    hitPosition = null; // clear the hit position - no multiple scoring 
 
     // speed up the moving target depending on score
     randomTargetPeriod = GAME_CONFIG.startPeriod - (score * 50); //set the new repeat period
@@ -102,13 +103,13 @@ function onTileClicked() {
 
 function isTargetHit() {
     // check if random target has been clicked or touched
-    //some code from 'Learn JavaScript by building 7 games: Whac-A-Mole' by Ania Kubow
+    // some code from 'Learn JavaScript by building 7 games: Whac-A-Mole' by Ania Kubow
     tiles.forEach(tile => {
         tile.addEventListener('click', () => {
-            // 'click' for mouse click or touch on touchscreen
+            // 'click' is used for mouse click or touch on touchscreen
             if (tile.id == hitPosition) {
-                onTileClicked();
-            } // if you hit the coloured box
+                onTileClicked(); // if target is hit
+            }
         });
     });
 }
@@ -123,10 +124,10 @@ function removeTarget() {
 }
    
 function resetGame() {
-    // reset game when Reset button is clicked/touched
+    // reset game when 'Replay' button is clicked/touched
     clearTheIntervals();
     scoreDisplayed.textContent = 0;
-    timeLeft.textContent = 60;
+    timeLeft.textContent = GAME_CONFIG.timePeriod;
 
     removeTarget(); // remove coloured tile and deactivate clicking
 
@@ -134,19 +135,24 @@ function resetGame() {
     const element = document.querySelector('#game-over');
     element.textContent = "";
 
-    start.disabled = false;
-    reset.disabled = true;
+    startGame();
+}
+
+function enableReplay() {
+    // 'Replay' button is active, whilst 'Start' is inactive
+    start.disabled = true;
+    reset.disabled = false;
 }
 
 function startGame() {
-    // start game when Start button is clicked/touched
+    // this is the first play of the game
     score = 0;
     period = GAME_CONFIG.timePeriod;
-    start.disabled = true;
-    reset.disabled = false;
+
+    enableReplay();
     
-    countDownTimer = setInterval(countDown, GAME_CONFIG.countDownPeriod); // every 1 sec
-    randomTargetTimer = setInterval(randomTarget, GAME_CONFIG.startPeriod);
+    countDownTimer = setInterval(countDown, GAME_CONFIG.countDownPeriod); // countdown every 1 sec
+    randomTargetTimer = setInterval(randomTarget, GAME_CONFIG.startPeriod); // create target initially every 1 sec
     
     isTargetHit(); // check if target is hit
 }
@@ -157,8 +163,5 @@ function gameOver() {
     gameOverText.textContent = "Game Over!";
 
     removeTarget(); // remove coloured tile and deactivate clicking
-
-    start.disabled = true;
-    reset.disabled = false;
+    enableReplay();
 }
-
